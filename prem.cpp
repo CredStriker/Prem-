1d2a96e5adef329e604213078cfc093fc59dafd8
CredStriker <149101396+CredStriker@users.noreply.github.com>
2024-03-12 10:40:04 +0530

    Create prem.cpp

---
+++ prem.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+ int a, b, c;
+cout<<"Prem Lal ------> 21BCS11722"<<endl;
+cout<<"Enter the two numbers to add"<<endl;
+cin>>a>>b;
+c=a+b;
+cout<<"The sum of"<<a<<"and"<<b<<"is"<<c<<endl;
+return 0;
+  }
